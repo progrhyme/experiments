b364d289d4ff27f57e7c4fbb10ac727456e3f82d
progrhyme <progrhyme@gmail.com>
2017-09-12 12:00:01 +0900

    [ruby-my_app] Workaround to avoid load error of 'database_rewinder'

--- ruby-my_app/spec/spec_helper.rb
+++ ruby-my_app/spec/spec_helper.rb
@@ -1,4 +1,9 @@
 require 'bundler'
 
 Bundler.setup(:default, :test)
+
+# NOTE: Workaround to avoid load error of 'database_rewinder'
+require 'rails'
+require 'active_record'
+
 Bundler.require(:default, :test)
